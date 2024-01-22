b0231ea57ac387bfb9c5c8fc751d71a6203ef463
Jeremy Payne <Jeremy.Payne@gov.ab.ca>
2024-01-22 12:03:40 -0700

    renamed variables

--- Core/Common/MappingProfile.cs
+++ Core/Common/MappingProfile.cs
@@ -1,14 +1,13 @@
 using AutoMapper;
 using WCDS.WebFuncions.Core.Model.Services;
 
-public class MappingProfile : Profile
+namespace WCDS.WebFuncions.Core.Common
 {
-    public MappingProfile()
+    public class MappingProfile : Profile
     {
-        CreateMap<CostDetailDto, CostDetail>()
-                .ForMember(dest => dest.ReportNumber, opt => opt.MapFrom(src => src.FlightReportId))
-                .ForMember(dest => dest.Date, opt => opt.MapFrom(src => src.FlightReportDate))
-                .ForMember(dest => dest.RegistrationNumber, opt => opt.MapFrom(src => src.ContractRegistrationName))
-                .ForMember(dest => dest.GlAccountNumber, opt => opt.MapFrom(src => src.Account));
+        public MappingProfile()
+        {
+            CreateMap<CostDetailDto, CostDetail>().ReverseMap();
+        }
     }
 }
--- Core/Model/Services/CostDetailDto.cs
+++ Core/Model/Services/CostDetailDto.cs
@@ -4,22 +4,22 @@ namespace WCDS.WebFuncions.Core.Model.Services
 {
     public class CostDetailDto
     {
-        public int FlightReportId { get; set; } //reportNumber
-        public DateTime FlightReportDate { get; set; }   //date
+        public int FlightReportId { get; set; }
+        public DateTime FlightReportDate { get; set; }
         public string Ao02Number { get; set; }
         public string Status { get; set; }
         public int ContractRegistrationId { get; set; }
-        public string ContractRegistrationName { get; set; } //registrationNumber
+        public string ContractRegistrationName { get; set; }
         public int ContractId { get; set; }
         public string ContractNumber { get; set; }
         public string VendorId { get; set; }
         public string VendorName { get; set; }
         public string FinanceVendorId { get; set; }
-        public string RateTypeId { get; set; } // rateUnit
+        public string RateTypeId { get; set; }
         public decimal NoOfUnits { get; set; }
         public string RateUnitId { get; set; }
         public decimal Cost { get; set; }
-        public string Account { get; set; }  // glAccountNumber
+        public string Account { get; set; }
         public string CostCenter { get; set; }
         public string FireNumber { get; set; }
         public string InternalOrder { get; set; }
