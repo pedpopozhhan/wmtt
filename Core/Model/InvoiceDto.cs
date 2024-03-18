9a870e75fff993deeee07e5bb1d4a2b4d4a488d6
Viral.Patel <Viral.Patel@V-VD-PD-0-1402>
2024-03-18 12:05:04 -0400

    model changes to remove redundant fields.

--- Core/Model/InvoiceDto.cs
+++ Core/Model/InvoiceDto.cs
@@ -18,20 +18,12 @@ namespace WCDS.WebFuncions.Core.Model
         public string PaymentStatus { get; set; }
         public string VendorBusinessId { get; set; }
         public string VendorName { get; set; }
-        public string AssignedTo { get; set; }
         public string ContractNumber { get; set; }
         public string Type { get; set; }
         public List<InvoiceTimeReportCostDetailDto> InvoiceTimeReportCostDetails { get; set; }
         public List<InvoiceOtherCostDetailDto> InvoiceOtherCostDetails { get; set; }
         public string UniqueServiceSheetName { get; set; }
-        public string PurchaseGroup { get; set; }
         public string ServiceDescription { get; set; }
-        public string CommunityCode { get; set; }
-        public string MaterialGroup { get; set; }
-        public string AccountType { get; set; }
-        public int Quantity { get; set; }
-        public string UnitOfMeasure { get; set; }
-        public decimal Price { get; set; }
         public string CreatedBy { get; set; }
         public DateTime? CreatedByDateTime { get; set; }
         public string UpdatedBy { get; set; }
