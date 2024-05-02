d85b7fd6208b8e0f219df4aef0d914ca57a1db18
Iftikhar Qamar <Iftikhar.Qamar@gov.ab.ca>
2024-05-02 09:58:37 -0600

    changed log types

--- Messages/InvoiceDataSyncMessageHandler.cs
+++ Messages/InvoiceDataSyncMessageHandler.cs
@@ -40,8 +40,6 @@ namespace WCDS.WebFuncions
                 client = new ServiceBusClient(Environment.GetEnvironmentVariable("InvoiceDataSyncTopicConnectionString"), new ServiceBusClientOptions() { TransportType = ServiceBusTransportType.AmqpWebSockets });
                 sender = client.CreateSender(Environment.GetEnvironmentVariable("InvoiceDataSyncTopicName"));
                 _log.LogInformation("SendCreateInvoiceMessage - started at:  {0} for invoice {1}", DateTime.UtcNow, invoiceNumber);
-                _log.LogError("SendCreateInvoiceMessage - started at:  {0} for invoice {1}", DateTime.UtcNow, invoiceNumber);
-                _log.LogDebug("SendCreateInvoiceMessage - started at:  {0} for invoice {1}", DateTime.UtcNow, invoiceNumber);
                 var message = new ServiceBusMessage
                 {
                     MessageId = Guid.NewGuid().ToString(),
@@ -49,8 +47,6 @@ namespace WCDS.WebFuncions
                     Body = new BinaryData(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(invoice)))
                 };
 
-                _log.LogInformation("SendCreateInvoiceMessage - Message: {0}", message.MessageId + "::" + message.Body);
-                _log.LogError("SendCreateInvoiceMessage - Message: {0}", message.MessageId + "::" + message.Body);
                 _log.LogDebug("SendCreateInvoiceMessage - Message: {0}", message.MessageId + "::" + message.Body);
 
                 const SslProtocols _Tls12 = (SslProtocols)0x00000C00;
@@ -60,9 +56,7 @@ namespace WCDS.WebFuncions
             }
             catch (Exception ex)
             {
-                _log.LogInformation("SendCreateInvoiceMessage - Error Sending a message: " + ex.Message + ex.InnerException);
                 _log.LogError("SendCreateInvoiceMessage - Error Sending a message: " + ex.Message + ex.InnerException);
-                _log.LogDebug("SendCreateInvoiceMessage - Error Sending a message: " + ex.Message + ex.InnerException);
                 throw new Exception("SendCreateInvoiceMessage - Error Sending a message: " + ex.Message + ex.InnerException);
             }
             finally
@@ -72,8 +66,6 @@ namespace WCDS.WebFuncions
             }
 
             _log.LogInformation("SendCreateInvoiceMessage - finished at: {0} for invoice {1}", DateTime.UtcNow, invoiceNumber);
-            _log.LogError("SendCreateInvoiceMessage - finished at: {0} for invoice {1}", DateTime.UtcNow, invoiceNumber);
-            _log.LogDebug("SendCreateInvoiceMessage - finished at: {0} for invoice {1}", DateTime.UtcNow, invoiceNumber);
         }
 
         public async Task SendUpdateInvoiceMessage(InvoiceDataSyncMessageDto invoice, string invoiceNumber)
@@ -92,7 +84,7 @@ namespace WCDS.WebFuncions
                     Body = new BinaryData(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(invoice)))
                 };
 
-                _log.LogInformation("SendUpdateInvoiceMessage - Message: {0}", message.MessageId + "::" + message.Body);
+                _log.LogDebug("SendUpdateInvoiceMessage - Message: {0}", message.MessageId + "::" + message.Body);
                 
                 const SslProtocols _Tls12 = (SslProtocols)0x00000C00;
                 const SecurityProtocolType Tls12 = (SecurityProtocolType)_Tls12;
