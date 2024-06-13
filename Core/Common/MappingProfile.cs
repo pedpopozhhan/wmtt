4d929b058661228212e044d50a31bc8ed8a4b0a1
Jeremy Payne <Jeremy.Payne@gov.ab.ca>
2024-06-13 16:48:07 -0600

    removed unused usings

--- Core/Common/MappingProfile.cs
+++ Core/Common/MappingProfile.cs
@@ -1,5 +1,3 @@
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
 using AutoMapper;
 using WCDS.WebFuncions.Core.Entity;
 using WCDS.WebFuncions.Core.Model;
